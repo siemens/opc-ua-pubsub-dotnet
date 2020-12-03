3143e5fb3f740c3e4158dd0b77b0e0789281cbdd
Piskol, Thomas <38790465+ThomasPiskol@users.noreply.github.com>
2020-12-03 17:41:02 +0100

    .net framework 4.8

--- src/Visualizer/Properties/Resources.Designer.cs
+++ src/Visualizer/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace opc.ua.pubsub.dotnet.visualizer.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon Siemens {
-            get {
-                object obj = ResourceManager.GetObject("Siemens", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }
