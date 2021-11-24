86083b1aaeafd24239ec8d7e0984be1c97cc7ccd
ThomasSeidenbecher <75260454+ThomasSeidenbecher@users.noreply.github.com>
2021-11-24 13:13:18 +0100

    Allow receiving of raw mqtt payload (#24)

--- src/Client/TestClient.cs
+++ src/Client/TestClient.cs
@@ -120,9 +120,15 @@ namespace opc.ua.pubsub.dotnet.client
 
         }
 
+        public void Subscribe( string topic, bool receiveRawData = false )
+        {
+            throw new NotImplementedException();
+        }
+
 #pragma warning disable 67
         public event EventHandler<Exception>  ExceptionCaught;
         public event FileReceivedEventHandler FileReceived;
+        public event RawDataReceivedEventHandler RawDataReceived;
 #pragma warning restore 67
     }
 }
